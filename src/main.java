3b7cd16df2e68c71fcd7e7d486ac3adfeebfc3f7
mfv <mit.fedor@gmail.com>
2020-09-03 20:59:25 +0500

    add transfer method

--- src/main.java
+++ src/main.java
@@ -1,6 +1,9 @@
 public class main {
     public static void main(String[] args) {
         int balance = 2_000_000_000;
-        int transfer = 500_000;
+        int transfer = 500_000_000;
+        int newBalance;
+        newBalance = balance + transfer;
+        System.out.println(newBalance);
     }
 }
